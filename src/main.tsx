7bcc6c513251abf133c041680d302759e04e53ec
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-03-10 00:11:39 +0530

    Changing scripts url

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+import "preline";
 
 const rootElement = document.getElementById("root");
 
